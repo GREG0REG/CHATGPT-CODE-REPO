0306b2765c38740c5757145b3bb9ade1e5bf6641
GREG0REG <g55282097@gmail.com>
2026-07-17 13:21:24 +0530

    Update backup_service.dart

--- lib/services/backup_service.dart
+++ lib/services/backup_service.dart
@@ -4,8 +4,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:workmanager/workmanager.dart';
 
-import '../db/database_helper.dart';        // ← FIXED: was 'db/database_helper.dart'
-import '../models/event.dart';              // ← ADDED: needed for Event type
+import '../database_helper.dart';
+import '../models/event.dart';
 
 const String kBackupTaskName = 'event_countdown_weekly_backup';
 
