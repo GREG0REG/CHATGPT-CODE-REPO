0e4b1d5877045d93a1607535feef585b9ad4593a
GREG0REG <g55282097@gmail.com>
2026-07-18 11:51:22 +0530

    Update recurrence_service.dart

--- lib/services/recurrence_service.dart
+++ lib/services/recurrence_service.dart
@@ -266,7 +266,7 @@ class RecurrenceService {
 
     // If we need more, generate from next year
     if (virtuals.length < maxCount) {
-      val nextYear = currentYear + 1;
+      final nextYear = currentYear + 1;
       final candidatesNextYear = specificDates.map((sd) {
         return _OccurrenceCandidate(sd.toDateTime(nextYear), sd);
       }).toList();
