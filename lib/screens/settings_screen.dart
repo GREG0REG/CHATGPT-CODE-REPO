49fc4d1d84ee25b0597593958f77abc8ee186c1c
GREG0REG <g55282097@gmail.com>
2026-07-21 22:57:43 +0530

    Add battery_plus package import to settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:battery_plus/battery_plus.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
