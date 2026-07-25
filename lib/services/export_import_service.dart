44ce5f9235cdfa6602b1cdfe0cc1afba8a731f34
GREG0REG <g55282097@gmail.com>
2026-07-25 18:48:39 +0530

    Update export_import_service.dart

--- lib/services/export_import_service.dart
+++ lib/services/export_import_service.dart
@@ -41,13 +41,17 @@ class ExportImportService {
     };
 
     final manifestJson = const JsonEncoder.withIndent('  ').convert(manifest);
+    final manifestBytes = utf8.encode(manifestJson);
+    final dataBytes = utf8.encode(dataJsonString);
 
     final archive = Archive()
-      ..addFile(ArchiveFile('manifest.json', manifestJson.length, utf8.encode(manifestJson)))
-      ..addFile(ArchiveFile('data.json', dataJsonString.length, utf8.encode(dataJsonString)));
+      ..addFile(ArchiveFile('manifest.json', manifestBytes.length, manifestBytes)
+        ..compressionLevel = 6)
+      ..addFile(ArchiveFile('data.json', dataBytes.length, dataBytes)
+        ..compressionLevel = 6);
 
     final zipEncoder = ZipEncoder();
-    final encoded = zipEncoder.encode(archive, level: 6);
+    final encoded = zipEncoder.encode(archive);
     if (encoded == null) {
       throw Exception('Failed to encode ZIP archive');
     }
