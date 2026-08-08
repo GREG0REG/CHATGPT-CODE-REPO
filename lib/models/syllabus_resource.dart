9b91a7f81d080f04eeaf048a5718eb8aceb02d22
GREG0REG <g55282097@gmail.com>
2026-08-08 10:04:22 +0530

    Update syllabus_resource.dart

--- lib/models/syllabus_resource.dart
+++ lib/models/syllabus_resource.dart
@@ -2,7 +2,7 @@ class SyllabusResource {
   final int? id;
   final int? topicId;
   final int? subtopicId;
-  final String resourceType; // 'file', 'link', etc.
+  final String resourceType;
   final String title;
   final String? filePath;
   final String? url;
@@ -19,25 +19,45 @@ class SyllabusResource {
     required this.createdAtMillis,
   });
 
+  SyllabusResource copyWith({
+    int? id,
+    int? topicId,
+    int? subtopicId,
+    String? resourceType,
+    String? title,
+    String? filePath,
+    String? url,
+    int? createdAtMillis,
+  }) => SyllabusResource(
+    id: id ?? this.id,
+    topicId: topicId ?? this.topicId,
+    subtopicId: subtopicId ?? this.subtopicId,
+    resourceType: resourceType ?? this.resourceType,
+    title: title ?? this.title,
+    filePath: filePath ?? this.filePath,
+    url: url ?? this.url,
+    createdAtMillis: createdAtMillis ?? this.createdAtMillis,
+  );
+
   Map<String, dynamic> toMap() => {
-        'id': id,
-        'topicId': topicId,
-        'subtopicId': subtopicId,
-        'resourceType': resourceType,
-        'title': title,
-        'filePath': filePath,
-        'url': url,
-        'createdAtMillis': createdAtMillis,
-      };
+    'id': id,
+    'topicId': topicId,
+    'subtopicId': subtopicId,
+    'resourceType': resourceType,
+    'title': title,
+    'filePath': filePath,
+    'url': url,
+    'createdAtMillis': createdAtMillis,
+  };
 
   factory SyllabusResource.fromMap(Map<String, dynamic> map) => SyllabusResource(
-        id: map['id'] as int?,
-        topicId: map['topicId'] as int?,
-        subtopicId: map['subtopicId'] as int?,
-        resourceType: map['resourceType'] as String,
-        title: map['title'] as String,
-        filePath: map['filePath'] as String?,
-        url: map['url'] as String?,
-        createdAtMillis: map['createdAtMillis'] as int,
-      );
+    id: map['id'] as int?,
+    topicId: map['topicId'] as int?,
+    subtopicId: map['subtopicId'] as int?,
+    resourceType: map['resourceType'] as String,
+    title: map['title'] as String,
+    filePath: map['filePath'] as String?,
+    url: map['url'] as String?,
+    createdAtMillis: map['createdAtMillis'] as int,
+  );
 }
