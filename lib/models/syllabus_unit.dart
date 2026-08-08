cb3ad9c514ba0bb02480f1312bf9657efddb8cfe
GREG0REG <g55282097@gmail.com>
2026-08-08 10:03:01 +0530

    Update syllabus_unit.dart

--- lib/models/syllabus_unit.dart
+++ lib/models/syllabus_unit.dart
@@ -15,21 +15,37 @@ class SyllabusUnit {
     required this.createdAtMillis,
   });
 
+  SyllabusUnit copyWith({
+    int? id,
+    int? subjectId,
+    String? name,
+    int? orderIndex,
+    int? weightage,
+    int? createdAtMillis,
+  }) => SyllabusUnit(
+    id: id ?? this.id,
+    subjectId: subjectId ?? this.subjectId,
+    name: name ?? this.name,
+    orderIndex: orderIndex ?? this.orderIndex,
+    weightage: weightage ?? this.weightage,
+    createdAtMillis: createdAtMillis ?? this.createdAtMillis,
+  );
+
   Map<String, dynamic> toMap() => {
-        'id': id,
-        'subjectId': subjectId,
-        'name': name,
-        'orderIndex': orderIndex,
-        'weightage': weightage,
-        'createdAtMillis': createdAtMillis,
-      };
+    'id': id,
+    'subjectId': subjectId,
+    'name': name,
+    'orderIndex': orderIndex,
+    'weightage': weightage,
+    'createdAtMillis': createdAtMillis,
+  };
 
   factory SyllabusUnit.fromMap(Map<String, dynamic> map) => SyllabusUnit(
-        id: map['id'] as int?,
-        subjectId: map['subjectId'] as int,
-        name: map['name'] as String,
-        orderIndex: map['orderIndex'] as int? ?? 0,
-        weightage: map['weightage'] as int?,
-        createdAtMillis: map['createdAtMillis'] as int,
-      );
+    id: map['id'] as int?,
+    subjectId: map['subjectId'] as int,
+    name: map['name'] as String,
+    orderIndex: map['orderIndex'] as int? ?? 0,
+    weightage: map['weightage'] as int?,
+    createdAtMillis: map['createdAtMillis'] as int,
+  );
 }
