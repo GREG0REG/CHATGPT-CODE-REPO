177fcbf60c3d2478d17199fc1573dcfe398ccc5f
GREG0REG <g55282097@gmail.com>
2026-08-08 12:11:44 +0530

    Update database_helper.dart

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -3418,14 +3418,6 @@ class DatabaseHelper {
     return maps.map((m) => SyllabusRevisionSchedule.fromMap(m)).toList();
   }
 
-  
-  Future<List<SyllabusRevisionSchedule>> getAllRevisionSchedules() async {
-    final db = await database;
-    final maps = await db.query('syllabus_revision_schedules', orderBy: 'scheduledDateMillis ASC');
-    return maps.map((m) => SyllabusRevisionSchedule.fromMap(m)).toList();
-   
-    }
-
   Future<void> generateRevisionSchedules(int topicId) async {
     final now = DateTime.now();
     final base = now.millisecondsSinceEpoch;
