7bd995bf7c9d92388bebb14efda11182909848dc
GREG0REG <g55282097@gmail.com>
2026-08-08 10:02:28 +0530

    Update syllabus_subject.dart

--- lib/models/syllabus_subject.dart
+++ lib/models/syllabus_subject.dart
@@ -15,21 +15,42 @@ class SyllabusSubject {
     required this.createdAtMillis,
   });
 
+  Color get color {
+    try {
+      final hex = colorHex.replaceFirst('#', '');
+      return Color(int.parse('FF$hex', radix: 16));
+    } catch (_) {
+      return const Color(0xFF2196F3);
+    }
+  }
+
+  SyllabusSubject copyWith({
+    int? id,
+    String? name,
+    String? colorHex,
+    int? targetCompletionDateMillis,
+    int? createdAtMillis,
+  }) => SyllabusSubject(
+    id: id ?? this.id,
+    name: name ?? this.name,
+    colorHex: colorHex ?? this.colorHex,
+    targetCompletionDateMillis: targetCompletionDateMillis ?? this.targetCompletionDateMillis,
+    createdAtMillis: createdAtMillis ?? this.createdAtMillis,
+  );
+
   Map<String, dynamic> toMap() => {
-        'id': id,
-        'name': name,
-        'colorHex': colorHex,
-        'targetCompletionDateMillis': targetCompletionDateMillis,
-        'createdAtMillis': createdAtMillis,
-      };
+    'id': id,
+    'name': name,
+    'colorHex': colorHex,
+    'targetCompletionDateMillis': targetCompletionDateMillis,
+    'createdAtMillis': createdAtMillis,
+  };
 
   factory SyllabusSubject.fromMap(Map<String, dynamic> map) => SyllabusSubject(
-        id: map['id'] as int?,
-        name: map['name'] as String,
-        colorHex: map['colorHex'] as String? ?? '#2196F3',
-        targetCompletionDateMillis: map['targetCompletionDateMillis'] as int?,
-        createdAtMillis: map['createdAtMillis'] as int,
-      );
-
-  Color get color => Color(int.parse(colorHex.replaceFirst('#', '0xff')));
+    id: map['id'] as int?,
+    name: map['name'] as String,
+    colorHex: map['colorHex'] as String? ?? '#2196F3',
+    targetCompletionDateMillis: map['targetCompletionDateMillis'] as int?,
+    createdAtMillis: map['createdAtMillis'] as int,
+  );
 }
