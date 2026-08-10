11117fdb24f514ec0727cec75f4178bbc1cfc6c7
GREG0REG <g55282097@gmail.com>
2026-08-10 10:12:12 +0530

    Add import for alarm_ring_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ import 'package:event_countdown/services/settings_service.dart';
 import 'package:event_countdown/services/widget_service.dart';
 import 'package:event_countdown/services/theme_service.dart';
 import 'package:event_countdown/theme/app_themes.dart';
+import 'package:event_countdown/screens/alarm_ring_screen.dart';
 
 const String kWidgetRefreshTaskName = 'event_countdown_widget_refresh';
 const String kWidgetRefreshFrequentTaskName = 'event_countdown_widget_frequent';
